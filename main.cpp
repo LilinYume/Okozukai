4fe6e049f466dd73028cf139678688fbb45ee782
LilinYume <resorcin@gmail.com>
2014-05-05 04:29:04 +0900

    main

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <cstdlib>
 #include <string>
+#include <vector>
 #include "time_stamp.h"
 
 void create_new_file( const char* file_name );
@@ -49,7 +50,11 @@ int main()
 	}
 	return 0;
 }
-
+/* ファイル名を今月の月名で作成・残高の入力を促す。
+	既に今月のファイルが作成済みならmain関数に制御を戻す。
+	ただし、ファイル自体が作成されていてもファイルサイズがゼロの場合残高の入力を促す。
+	入力値は整数値のみを許容。
+	入力値がゼロまたはEOFがの場合はプログラムを終了する								　*/
 void create_new_file( const char* file )
 {
 	int balance = -1;
@@ -74,6 +79,9 @@ void create_new_file( const char* file )
 	}
 	fs_out << "balance: " << balance << std::endl;
 }
+/* ファイルが空かどうかを調べる
+	ファイルの先頭から1文字取得し、即座にEOFが帰ってくればファイルが空（サイズ・ゼロ）とみなす。
+	ファイルが空の場合 True を返し、ファイルが空でなければ False を返す。					*/
 bool is_empty_file( const char* file )
 {
 	std::ifstream fs_in( file, std::ios::binary );
@@ -87,6 +95,9 @@ bool is_empty_file( const char* file )
 	if ( status == EOF ) return true;
 	return false;
 }
+/* 整数値の入力を促す
+	入力ストリームのステータスが Good状態になるまで再入力を促し、
+	入力された整数値を返す													 */
 int inpt( const char* prompt_messeage ) 
 {
 	int inpt_val = 0;
@@ -95,8 +106,12 @@ int inpt( const char* prompt_messeage )
 		std::cout << prompt_messeage;
 		std::cin >> inpt_val;
 	} while( inpt_status( std::cin ) == NG );
+
 	return inpt_val;
 }
+/* 整数値の入力を促す
+	入力ストリームのステータスを引数で参照渡しする。
+	入力された整数値を返す													 */
 int inpt( const char* prompt_messeage, int& input_status )
 {
 	int inpt_val = -1;
@@ -118,6 +133,12 @@ int inpt( const char* prompt_messeage, int& input_status )
 	else input_status = OK;
 	return inpt_val;
 }
+/* 入力ストリームのストリーム状態を返す
+	EOF なら シンボル定数 EOF: -1
+	Fail なら シンボル定数 NG: 0
+	Good なら シンボル定数 OK: 1
+	をそれぞれ返す。
+*/
 int inpt_status( std::istream& stream_in ) 
 {
 	if ( stream_in.eof() ) {
@@ -133,6 +154,10 @@ int inpt_status( std::istream& stream_in )
 	}
 	return OK;
 }
+/* ディスク・ファイルに入力された整数値を書き込む。
+	ファイルへ書き込む前に書き込み確認をする。
+	ファイル名は今月の月名
+	フォーマット：日付 [space] 曜日 [space] 入力値 [LF]			*/
 void write_to_disk( const char* file, int write_val )
 {
 	if ( write_val == 0 ) { 
@@ -160,6 +185,11 @@ void write_to_disk( const char* file, int write_val )
 					<< "type command: ";
 	}
 }
+/* ファイルへの書き込み確認
+	YesかNoを入力させる。
+	入力値（char）は y Y n N のいずれかに収まるようにする。
+	Yes('y' または 'Y' の入力)なら Trueを返す。
+	No( 'n' または 'N' の入力)なら Falseを返す。				*/
 bool write_confirm() 
 {
 	char chose = '\0';
@@ -181,10 +211,16 @@ bool write_confirm()
 	}
 	return false;
 }
+/*	支出の履歴を表示する
+	 今月の月名ファイルを開きファイル内の値をコンソール上に出力。
+	 また、出費の合計を計算し、出費合計も同様に出力する。				*/
 void view_history( const char* file )
 {
-	std::string balance, rows;
-	balance = rows = "";
+	int amount = 0;
+	std::vector<std::string> sum_of_cost;
+	std::string balance, date, week, costs;
+	balance = date = week = "";
+	
 
 	std::ifstream fs_in ( file, std::ios::binary );
 	if ( !fs_in ) {
@@ -194,9 +230,15 @@ void view_history( const char* file )
 	getline( fs_in, balance );
 	std::cout << balance << std::endl;
 	
-	while( getline( fs_in, rows ) ) {
-		std::cout << rows << std::endl;
+	while(  fs_in >> date >> week >> costs  ) {
+		std:: cout << date << ' ' << week << ' ' << costs << std::endl;
+		sum_of_cost.push_back( costs );
+	}
+
+	for ( int i = 0; i < sum_of_cost.size(); ++i ) {
+		amount += atoi( sum_of_cost[i].c_str() );
 	}
 	std::cout << "----------------------------------" << std::endl;
+	std::cout << "total costs: " << amount << std::endl;
 	std::cout << "type command: ";
 }
